70cd547dacdcd453128fcef69c7799d858462fb3
RamananSri <1058515@ucn.dk>
2017-11-24 00:53:59 +0100

    project

---
+++ ClientWPF/Models/District.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWPF.Models
+{
+    public class District
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Store> Stores { get; set; }
+        public SalesPerson PrimarySalesPerson { get; set; }
+        public List<SalesPerson> SecondarySalesPersonel { get; set; }
+    }
+}
---
+++ ClientWPF/ViewModels/DistrictViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ClientWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWPF.ViewModels
+{
+    public class DistrictViewModel
+    {
+        public ObservableCollection<District> Districts { get; set; }
+        public District SelectedDistrict { get; set; }
+
+        public DistrictViewModel()
+        {
+            Store s1 = new Store { ID = 1, Name = "Fakta" };
+            Store s2 = new Store { ID = 2, Name = "Bilka" };
+
+
+
+        }
+
+    }
+}
